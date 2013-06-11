a0b71db1f4e42c6c5d22cf207604fec6ceee68ab
johnny <j.e.miyake@gmail.com>
2013-06-11 16:44:25 +0700

    added sleep

--- app/workers/import_csv.rb
+++ app/workers/import_csv.rb
@@ -4,6 +4,7 @@ class ImportCsv
   def self.perform(file)
     require "csv"
     CSV.foreach(file, headers: true) do |row|
+      sleep 30
       Client.create(
         client_name: row[0],
         roman_name: row[1],
